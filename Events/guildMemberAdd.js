97d8fa39b8017c3d857f9b2807d722701fbaf866
Niko Shefiyev <84826363+n1k0shefiyev@users.noreply.github.com>
2021-11-03 06:45:11 +0400

    Add files via upload

---
+++ Events/guildMemberAdd.js
@@ -0,0 +1,39 @@
+const ayar = require('../settings.js');
+const Discord = require('discord.js');
+const moment = require('moment')
+require('moment-duration-format');
+const registerData = require('../models/register.js');
+module.exports = async member => {
+        let data = await registerData.findOne({ guildID: member.guild.id })
+        let kurulus = member.user.createdTimestamp
+        let süphe;
+        if (Date.now() - kurulus < 1000 * 60 * 60 * 24 * 10 ? süphe = "Şüpheli" : süphe = "Güvenli");
+
+        let olusturma = `(\`${moment.duration(Date.now() - kurulus).format('Y [yıl], M [Ay], D [Gün]')}\`)`
+        let channel = member.guild.channels.cache.get(ayar.channels.registerChannel);
+
+        if (süphe === "Güvenli") {
+            await member.roles.add(ayar.roles.unregisterRoles).catch(e => {});
+            await member.setNickname(ayar.guild.defaultName).catch(e => {});
+            if (channel) channel.send(`
+Sunucumuza hoş geldin ${member} - \`${member.id}\`
+        
+Hesabın **${moment(kurulus).locale('tr').format('LLL')}** tarihinde ${olusturma} önce oluşturulmuş! ${member.guild.emojis.cache.get(ayar.emojis.yes)}
+        
+Sunucu kurallarımız ${member.guild.channels.cache.get(ayar.channels.rulesChannel)} kanalında belirtilmiştir. Unutma sunucu içerisinde ki ceza işlemlerin kuralları okuduğunu varsayarak gerçekleştirilecek.
+
+Sunucumuza kayıt olmak için ${data ? data.tagMode === true ? `tagımızı alıp ses teyit vermelisin.` : `ses teyit vermelisin.` : `ses teyit vermelisin.`}
+                
+Seninle beraber **${member.guild.memberCount}** kişiyiz! İyi eğlenceler. <a:hosgeldinKonfeti:895015925504426027> <a:hosgeldinKonfeti:895015925504426027> <a:hosgeldinKonfeti:895015925504426027>
+
+<#894935958338818068> **Kanalına girerek saniyeler içerisinde <@&894935902495866901> Yetkili Arkadasımız Sizi kayıt Edecektir.**    
+
+`);
+    } else {
+await member.roles.set([ayar.roles.suspecious]).catch(e => {});
+await member.setNickname(ayar.guild.suspeciousName).catch(e => {});
+
+if(channel) channel.send(`
+${member}, Adlı kullanıcı sunucuya katıldı fakat hesabı yeni olduğu için şüpheli hesap rolünü verdim. ${olusturma}`);
+    }
+};
---
+++ settings.js
@@ -0,0 +1,52 @@
+module.exports = {
+    bot: {
+        botOwner: ["619126609756160000"],//Bot Sahibi İdiniz
+        botToken: "",//Botun Tokenı 
+        botPrefix: ".",//Botun Prefixi
+        botStatus: "M A R İ N O ❤️ PUSHA",//Bot Durumu Kafanıza Göre Girin
+        mongoURL: "",//Mongo Url Gireceksiniz Buraya
+    },
+    roles: {
+        ChatSorumlusu: ["894935865233657867"],//yetkili Hammer İd
+        PublicSorumlusu: ["894935865233657867"],//yetkili Hammer İd
+        KayıtSorumlusu: ["894935865233657867"],//yetkili Hammer İd
+        İnviteSorumlusu: ["894935865233657867"],//yetkili Hammer İd
+        
+        ChatSorumlusuRole: "894935890231709797",//ChatSorumlusu Rol İd
+        PublicSorumlusuRole: "894935891959767041",//PublicSorumlusu Rol İd
+        KayıtSorumlusuRole: "894935891297071145",//KayıtSorumlusu Rol İd
+        İnviteSorumlusuRole: "894935892739899463",//İnviteSorumlusu Rol İd
+
+
+
+        vipStaff: ["894935899866034268"],//Vip Hammer İd
+        registerStaff: ["894935902495866901"],//Register Hammer İd
+        manRoles: ["894935920913027123","894935921819000883"], // erkek rolleri
+        womanRoles: ["894935917851197492","894935919260495942"], // kız rolleri
+        unregisterRoles: ["894935923131822101"],//Kayıtsız İd
+        tagRole: "894935907222831105",//Tag Rol İd
+        vipRole: "894935908644683828",//Vip Rol İd
+        boosterRole: "784640746983784458",//Booster Rol İd
+        suspecious: "894935933965713468" // şüpheli hesap rolü
+    },
+    channels: {
+        registerChannel: "894935981315227668",//Hosgeldin Kanalı İd
+        rulesChannel: "894935985941532712",//Kurallar Kanalı İd
+        botVoice: "",// Botun Sese Girecek Kanal İd
+
+    },
+    guild: {
+        guildID: "774977651429212181",//Sunuucu İd
+        tag: "✰", // BURAYA İSMİN BAŞINA GELECEK TAG BİRDEN ÇOK TAGINIZ VARSA AŞŞAĞIYA GİRİN
+        tagges: ["✰",], // BİRDEN ÇOK TAGINIZ VARSA BURAYA GİRİN ÖRNEK: #0001
+        defaultTag: "•",
+        defaultName: "• İsim Yaş",
+        suspeciousName: "• Şüpheli Hesap",
+    },
+    emojis: {
+        yes: "881582775617069106",
+        no: "784635066906705920",
+        // EMOJİLERİN ID GİRCEKSİNİZ SADECE ID
+    }
+
+};
